a94be758cd90a6fda1d50bea247039c9fb7c7b05
SANTISALGADO <santisalgado200@outlook.com>
2025-08-27 15:55:49 -0500

    Noshecuacua

---
+++ src/main/java/com/example/demo/java1/ListaDeseos/ListaDeseos.java
@@ -0,0 +1,35 @@
+package com.example.demo.java1.ListaDeseos;
+
+import java.time.LocalDate;
+
+public class ListaDeseos {
+
+    private int idLista;
+    private int idCliente;
+    private LocalDate fechaCreacion; // ✅ camelCase en vez de fecha_creacion
+
+    public int getIdLista() {
+        return idLista;
+    }
+
+    public void setIdLista(int idLista) {
+        this.idLista = idLista;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDate getFechaCreacion() {   // ✅ cambia a camelCase
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {  // ✅ camelCase
+        this.fechaCreacion = fechaCreacion;
+    }
+}
+
