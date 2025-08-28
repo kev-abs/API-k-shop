24625b5eb4a73ea5f17bf003b5ad74256b285876
user.email <manuel25142808@gmail.com>
2025-08-28 15:40:58 -0500

    tabla proveedor

--- src/main/java/com/example/demo/java1/proveedor/Proveedor.java
+++ src/main/java/com/example/demo/java1/proveedor/Proveedor.java
@@ -1,4 +1,58 @@
-package com.example.demo.java1.proveedor;
+package com.example.demo.java1.Proveedor;
 
 public class Proveedor {
+    private int ID_Proveedor;
+    private String Nombre_Empresa;
+    private String Contacto;
+    private long Telefono;
+    private String Correo;
+    private String Direccion;
+
+    public int getID_Proveedor() {
+        return ID_Proveedor;
+    }
+
+    public void setID_Proveedor(int ID_Proveedor) {
+        this.ID_Proveedor = ID_Proveedor;
+    }
+
+    public String getNombre_Empresa() {
+        return Nombre_Empresa;
+    }
+
+    public void setNombre_Empresa(String nombre_Empresa) {
+        Nombre_Empresa = nombre_Empresa;
+    }
+
+    public String getContacto() {
+        return Contacto;
+    }
+
+    public void setContacto(String contacto) {
+        Contacto = contacto;
+    }
+
+    public long getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        Telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
 }
