5398a3f3708860bfe07af0f5f22fc1d5bf8c6827
Kevin-Alexis-Mendez-Vega <kevinmendez5478@gmail.com>
2025-08-29 14:58:51 -0500

    Cambios en ingreso compra

--- src/main/java/com/example/demo/java1/Proveedor/ConexionServiceProveedor.java
+++ src/main/java/com/example/demo/java1/Proveedor/ConexionServiceProveedor.java
@@ -1,4 +1,4 @@
-package com.example.demo.java1.proveedor;
+package com.example.demo.java1.Proveedor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
--- src/main/java/com/example/demo/java1/proveedor/Proveedor.java
+++ src/main/java/com/example/demo/java1/proveedor/Proveedor.java
@@ -1,4 +1,4 @@
-package com.example.demo.java1.proveedor;
+package com.example.demo.java1.Proveedor;
 
 public class Proveedor {
     private int ID_Proveedor;
