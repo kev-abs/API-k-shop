26556c796da3ae89fece8d6d90942d5b0e152c8e
Kevin Mendez <kevinmendez5478@gmail.com>
2025-12-16 10:58:18 -0500

    Obetener Checkout

--- src/main/java/com/example/demo/java1/Ventas/Checkout/CheckoutController.java
+++ src/main/java/com/example/demo/java1/Ventas/Checkout/CheckoutController.java
@@ -3,6 +3,8 @@ package com.example.demo.java1.Ventas.Checkout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/checkout")
 @CrossOrigin
@@ -10,6 +12,11 @@ public class CheckoutController {
 
     @Autowired
     private CheckoutService checkoutService;
+
+    @GetMapping("/cliente/{idCliente}")
+    public List<Checkout> historialPorCliente(@PathVariable int idCliente) {
+        return checkoutService.obtenerPorCliente(idCliente);
+    }
 
     @PostMapping
     public void guardarCheckout(@RequestBody Checkout checkout) {
