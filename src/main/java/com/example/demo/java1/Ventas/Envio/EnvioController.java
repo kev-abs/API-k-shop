1d96801117d0f96c654f9ab6e99f78c206fdd5fe
Kevin Mendez <kevinmendez5478@gmail.com>
2026-02-06 02:49:08 -0500

    Se realizo todo lo relacionado al carrito y a pedidos

---
+++ src/main/java/com/example/demo/java1/Ventas/Envio/EnvioController.java
@@ -0,0 +1,30 @@
+package com.example.demo.java1.Ventas.Envio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/envio")
+@CrossOrigin
+public class EnvioController {
+
+    @Autowired
+    private EnvioService envioService;
+
+    // Cliente consulta estado
+    @GetMapping("/pedido/{idPedido}")
+    public Envio obtenerPorPedido(@PathVariable int idPedido) {
+        return envioService.obtenerPorPedido(idPedido);
+    }
+
+    // Admin cambia estado
+    @PutMapping("/{idEnvio}/estado/{estado}")
+    public void cambiarEstado(
+            @PathVariable int idEnvio,
+            @PathVariable String estado) {
+
+        envioService.cambiarEstado(idEnvio, estado);
+    }
+}
+
