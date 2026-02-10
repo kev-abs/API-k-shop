983037bc9a558245b212a1f0d770be8154228e41
Kevin Mendez <kevinmendez5478@gmail.com>
2026-02-10 12:42:32 -0500

    Actualización en ServiceProductos

--- src/main/java/com/example/demo/java1/Productos/producto/ServiceProductos.java
+++ src/main/java/com/example/demo/java1/Productos/producto/ServiceProductos.java
@@ -3,8 +3,12 @@ package com.example.demo.java1.Productos.producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 
@@ -41,17 +45,27 @@ public class ServiceProductos {
 
     // Insertar producto (sin imagen binaria, solo nombre)
     public void insertarProducto(Producto producto) {
+
         String sql = "INSERT INTO producto (Nombre, Descripcion, Precio, Stock, ID_Proveedor, Imagen, Estado) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql,
-                producto.getNombre(),
-                producto.getDescripcion(),
-                producto.getPrecio(),
-                producto.getStock(),
-                producto.getID_Proveedor(),
-                producto.getImagen(),
-                producto.getEstado()
-        );
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, producto.getNombre());
+            ps.setString(2, producto.getDescripcion());
+            ps.setDouble(3, producto.getPrecio());
+            ps.setInt(4, producto.getStock());
+            ps.setInt(5, producto.getID_Proveedor());
+            ps.setString(6, producto.getImagen());
+            ps.setString(7, producto.getEstado());
+            return ps;
+        }, keyHolder);
+
+        if (keyHolder.getKey() != null) {
+            producto.setID_Producto(keyHolder.getKey().intValue());
+        }
     }
 
     // Actualizar producto
